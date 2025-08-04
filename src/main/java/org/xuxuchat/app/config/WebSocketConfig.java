253d840ff399f56b0a9d782e9e11d63dcbd70ccb
DaniloLO53 <daniloleaodev@gmail.com>
2025-08-04 18:46:14 -0300

    [refactor] deleted message converter

--- src/main/java/org/xuxuchat/app/config/WebSocketConfig.java
+++ src/main/java/org/xuxuchat/app/config/WebSocketConfig.java
@@ -1,17 +1,10 @@
 package org.xuxuchat.app.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.messaging.converter.DefaultContentTypeResolver;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,24 +13,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setUserDestinationPrefix("/user");
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/user");
+        registry.enableSimpleBroker("/user"); // TODO: adicionar "/topic" para mensagens em grupos
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
-    }
-
-    @Override
-    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
-        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
-
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setObjectMapper(new ObjectMapper());
-        converter.setContentTypeResolver(resolver);
-        messageConverters.add(converter);
-
-        return false;
     }
 }
